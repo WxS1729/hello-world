b49d592a1139d5deaeb961f0ca888d6fe9a5a10e
weixvshun <1595849283@qq.com>
2024-01-03 19:47:10 +0800

    修改

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("helloWorld");
+        System.out.println("==================================");
 
-        System.out.println("这是第二次修改");
     }
 }
 
